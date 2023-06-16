129c3ad646b36e383ad37f1fb18824ef6a2cedf3
dendywool <zaharkhachaturyan@gmail.com>
2023-06-16 14:39:59 +0200

    first commit website layout only

---
+++ src/components/app/App.js
@@ -0,0 +1,27 @@
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+
+import decoration from '../../resourses/img/vision.png'
+
+function App() {
+  return (
+    <div className="app">
+      <AppHeader />
+
+      <main>
+        <RandomChar />
+
+        <div class="char__content">
+          <CharList />
+          <CharInfo />
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appBanner/AppBanner.js
@@ -0,0 +1,19 @@
+import "./addBanner.scss";
+import avengers from '../../resourses/img/Avengers.png';
+import avengersLogo from '../../resourses/img/Avengers_logo.png'
+
+
+const AppBanner = () => {
+    return (
+        <div className="app__banner">
+            <img src={avengers} alt="Avengers"/>
+            <div className="app__banner-text">
+                New comics every week!<br/>
+                Stay tuned!
+            </div>
+            <img src={avengersLogo} alt="Avengers logo"/>
+        </div>
+    )
+}
+
+export default AppBanner;
